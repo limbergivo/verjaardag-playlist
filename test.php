5cf7a6536fa82c42cd9ac2a1c17bce3079b00ae5
ThomasNys <44265862+ThomasNys@users.noreply.github.com>
2019-01-24 10:27:07 +0100

    Add Stable PHP with user Input

--- test.php
+++ test.php
@@ -1,30 +1,38 @@
 
 <?php
+// Proper headers
+header('Content-Type: application/x-www-form-urlencoded');
+
+// ServerInfo
 $servername = "localhost";
 $username = "root";
 $password = "BirthDayApp";
 $dbname = "SONGS";
 
-$TitleP = "testTitle";
-$AuthorP = "testAuthor";
-$UrlP = "testUrl";
-$MessageP = "testMessage";
-$UserP =  "testUser";    /* mysql_real_escape_string($_POST["formGroupExampleInput"]); */
-$EmailP = "testEmail";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
    die("Connection failed: " . $conn->connect_error);
 }
-
-$query="INSERT INTO Songs (Title, Author, Url, Message, User, Email) VALUES ('".$TitleP."' , '".$AuthorP."' , '".$UrlP."' , '".$MessageP."' , '".$UserP."' , '".$EmailP."')";
-
-if ($conn->query($query) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $query . "<br>" . $conn->error;
+// Prep query
+$InsertQuery = "INSERT INTO Songs (Title, Author, Url, Message, User, Email) VALUES (?, ?, ?, ?, ?, ?)";
+if ($stmt = $conn->prepare($InsertQuery)){
+    $stmt->bind_param('ssssss', $TitleP, $AuthorP, $UrlP, $MessageP, $UserP, $EmailP);
+// Set variables ::currently only Post USR field in Title field
+    $TitleP = $_POST["USR"];
+    $AuthorP = "testAuthor";
+    $UrlP = "testUrl";
+    $MessageP = "testMessage";
+    $UserP =  "TestUser";
+    $EmailP = "testEmail";
+// Execute query
+$stmt->execute();
+$stmt->close();
 }
-
+// Error file
+else{echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;}
+// Close Connection
 $conn->close();
+
 ?>
