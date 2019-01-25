ec17c182e67e7e9d8d72a5d3b900d5b623f9756b
ThomasNys <44265862+ThomasNys@users.noreply.github.com>
2019-01-25 10:50:29 +0100

    add If song unique check

--- test.php
+++ test.php
@@ -13,25 +13,39 @@ $dbname = "SONGS";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
-// Prep query
-$InsertQuery = "INSERT INTO Songs (Title, Author, Url, Message, User, Email) VALUES (?, ?, ?, ?, ?, ?)";
-if ($stmt = $conn->prepare($InsertQuery)){
-    $stmt->bind_param('ssssss', $TitleP, $AuthorP, $UrlP, $MessageP, $UserP, $EmailP);
-// Set variables ::currently only Post USR field in Title field
-    $TitleP = $_POST["USR"];
-    $AuthorP = "testAuthor";
-    $UrlP = "testUrl";
+
+$TitleP = $_POST["Titel"];
+if($result = mysqli_query($conn, "SELECT * FROM Songs WHERE `Title` = '$TitleP'")){
+    echo mysqli_num_rows("rows "+$result);
+if (mysqli_num_rows($result) > 0)
+{
+    echo "Title already added";
+}
+else
+{
+    echo 'Unique Song';
+    $AuthorP = $_POST["Artiest"];
+    $UrlP = $_POST["Youtube"];;
     $MessageP = "testMessage";
     $UserP =  "TestUser";
     $EmailP = "testEmail";
-// Execute query
-$stmt->execute();
-$stmt->close();
+
+    // Prep query
+    $InsertQuery = "INSERT INTO Songs (Title, Author, Url, Message, User, Email) VALUES (?, ?, ?, ?, ?, ?)";
+    if ($stmt = $conn->prepare($InsertQuery)){
+        $stmt->bind_param('ssssss', $TitleP, $AuthorP, $UrlP, $MessageP, $UserP, $EmailP);
+        // Set variables ::currently only Post USR field in Title field
+        // Execute query
+        $stmt->execute();
+        $stmt->close();
+    }
+    // Error file
+    else{echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;}
+
 }
-// Error file
-else{echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;}
+}
 // Close Connection
 $conn->close();
 
